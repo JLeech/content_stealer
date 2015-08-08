9f3476c1178f1e6dc1e9fac5d59efaef67c88ec6
Eve <eveleech@gmail.com>
2015-08-08 16:28:07 +0300

    9gag img save for work ignoring

--- ninegag.rb
+++ ninegag.rb
@@ -19,9 +19,11 @@ class Ninegag
 			points = news_block.css("span.badge-item-love-count").text.gsub(/(,|\s)/,"").to_i
 			if points > 10000
 				news_name = news_block.css("header").css("a.badge-evt").text.strip
-				picture_url = news_block.css("img.badge-item-img")[0]["src"]
-				news_link = news_block["data-entry-url"]
-				@news << [picture_url,news_name,points, news_link]
+				unless news_block.css("img.badge-item-img").empty?
+					picture_url = news_block.css("img.badge-item-img")[0]["src"]
+					news_link = news_block["data-entry-url"]
+					@news << [picture_url,news_name,points, news_link]
+				end
 			end
 		end
 		Helper.process_saving(@news, self.class)
